8e16440d45c0f4169a69f4cc472d6ee5520150d1
Rohit Nair <ronair@gmail.com>
2018-12-23 09:14:42 -0700

    Reorganizing directory structure. Added help text to script to print out usage.

--- scripts/createartefacts.sh
+++ scripts/createartefacts.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+usage="$(basename "$0") [-h|--help] [-d|--day=n]
+Creates a day script file and a testinput and input file for the day argument passed.
+
+where:
+    -h|--help   Shows this help text
+    -d|--day    Set the day value."
+
 for i in "$@"
 do
 case $i in
     -d=*|--day=*)
     DAY="${i#*=}"
+
+    ;;
+    -h | --help)
+    echo "$usage"
+    exit
 
     ;;
    --default)
@@ -17,6 +29,11 @@ esac
 done
 
 cd "${0%/*}"
-echo $PWD
+echo "Path="$PWD "Day="$DAY
 
-mkdir ../python/day${DAY} && cd ../python/day${DAY} && touch ${DAY}.py && touch input${DAY}.txt && touch testinput${DAY}.txt && chmod +x ${DAY}.py
+mkdir ../2018/python/day${DAY} && 
+    cd ../2018/python/day${DAY} && 
+    touch ${DAY}.py && 
+    touch input${DAY}.txt && 
+    touch testinput${DAY}.txt && 
+    chmod +x ${DAY}.py
